99171743b51d83f827529508cb1cdd103a094203
jojo <jojo@duolin.com>
2025-10-24 16:18:59 +0800

    点击侧边菜单拿到路径

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -44,29 +44,33 @@ const View: React.FC = () => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  
+  const menuClick = (e:{key:string}) =>{
+    console.log('点击了菜单', e.key);
+    // 点击跳转到对应的路由
+  }
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
+        {/* 左边 */}
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
         <div className="demo-logo-vertical" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+        {/* 左边列表 */}
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} onClick={menuClick}/>
       </Sider>
+        {/* 右边 */}
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
-        <Content style={{ margin: '0 16px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }} items={[{ title: 'User' }, { title: 'Bill' }]} />
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            Bill is a cat.
-          </div>
+        {/* 右边头部 */}
+        <Header style={{ paddingLeft: "16px", background: colorBgContainer }} >
+        {/* 右边头部面包屑 */}
+        <Breadcrumb style={{ lineHeight:'64px' }} items={[{ title: 'User' }, { title: 'Bill' }]} />
+        </Header>
+        {/* 右边内容部分-白色底盒子 */}
+        <Content style={{ margin: '16px 16px 0' , padding: 24, minHeight: 360, background: colorBgContainer, borderRadius: borderRadiusLG,}}>
+            {/* 窗口 */}
         </Content>
-        <Footer style={{ textAlign: 'center' }}>
+        {/* 右边底部 */}
+        <Footer style={{ textAlign: 'center', padding:0, lineHeight:'48px' }}>
           Ant Design ©{new Date().getFullYear()} Created by Ant UED
         </Footer>
       </Layout>
