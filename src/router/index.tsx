e82d42d69558143bf496bc658568bee58cda9017
jojo <jojo@duolin.com>
2025-10-24 14:33:52 +0800

    Route老写法:重定向

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,15 @@
 import App from '../App'
 import Home from '../views/Home'
 import About from '../views/About'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
 // 两种路由模式的组件： BrowserRouter（History模式），HashRouter（Hash模式）
 
 const baseRouter = () => (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App/>}>
+                {/*  Navigate 重定向*/}
+                <Route path="/" element={<Navigate to="/home"/>}></Route>
                 <Route path="home" element={<Home/>}></Route>
                 <Route path="about" element={<About/>}></Route>
             </Route>
