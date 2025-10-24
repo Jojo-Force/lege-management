a410e5fc50fd68dc69204158741b714762413032
jojo <jojo@duolin.com>
2025-10-24 15:47:13 +0800

    Home界面Layout创建

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,15 @@ function App() {
   const outlet = useRoutes(router)
   return (
       <div className="App">
-        顶级组件
-        <Comp1/>
+        {/* 顶级组件 */}
+        {/* <Comp1/>
         <Comp2/>
         <Button type="primary">Primary Button</Button>
-        <UpOutlined style={{ fontSize:'10px',color:'red'}}/>
+        <UpOutlined style={{ fontSize:'10px',color:'red'}}/> */}
         {/* 占位符组件 */}
-        <Link to="/home">Home</Link>
+        {/* <Link to="/home">Home</Link>
         <Link to="/about">About</Link>
-        <Link to="/user">User</Link>
+        <Link to="/user">User</Link> */}
         {outlet}
       </div>
   )
