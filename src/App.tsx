e1fe73c0a7c9223afd8039aecd95e4fbb79ed42d
jojo <jojo@duolin.com>
2025-10-24 10:59:54 +0800

    1.Antd初步引入

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import Comp1 from "@/components/Comp1"
 import Comp2 from "@/components/Comp2"
 
+import { UpOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +14,8 @@ function App() {
         顶级组件
         <Comp1/>
         <Comp2/>
+        <Button type="primary">Primary Button</Button>
+        <UpOutlined style={{ fontSize:'10px',color:'red'}}/>
       </div>
   )
 }
