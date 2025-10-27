fa1ff43f882893431a22f77b2273eab58020484e
jojo <jojo@duolin.com>
2025-10-27 08:32:00 +0800

    1.完成登录页的设计

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,6 +1,6 @@
 import style from './login.module.scss'
 import initLoginBg from './init.ts'
-import { useEffect } from 'react'
+import { useEffect, useState, type ChangeEvent } from 'react'
 import { Button, Input,Space } from 'antd';
 import './login.less'
 const View = () => {
@@ -11,6 +11,28 @@ const View = () => {
         window.onresize= function(){initLoginBg()};
     },[])
 
+    //获取用户输入的信息
+    const [usernameVal,setUsernameVal] = useState("");//定义用户名信息变量
+    const [passwordVal,setPasswordVal] = useState("");//定义密码信息变量
+    const [captchVal,setCaptchVal] = useState("");//定义密码信息变量
+    const usernameChange = (e:ChangeEvent<HTMLInputElement>)=>{
+        //console.log(e.target.value)
+        setUsernameVal(e.target.value);
+    }
+
+    const passwordChange = (e:ChangeEvent<HTMLInputElement>)=>{
+        //console.log(e.target.value)
+        setPasswordVal(e.target.value);
+    }
+
+    const captchChange = (e:ChangeEvent<HTMLInputElement>)=>{
+        //console.log(e.target.value)
+        setCaptchVal(e.target.value);
+    }
+
+    const goLogin = () =>{
+        console.log("用户输入的用户名，密码，验证码分别是：",usernameVal,passwordVal,captchVal)
+    }
     return (
         <div className={style.loginPage}>
             {/* 存放背景 */}
@@ -26,9 +48,15 @@ const View = () => {
                 {/* 表单部分 */}
                 <div className='form'>
                     <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-                    <Input placeholder="用户名" />
-                    <Input.Password placeholder="密码" />
-                    <Button type="primary" block>登录</Button>
+                    <Input placeholder="用户名" onChange={usernameChange}/>
+                    <Input.Password placeholder="密码" onChange={passwordChange}/>
+                    <div className={style.captchaBox}>
+                        <Input placeholder="验证码" onChange={captchChange}/>
+                        <div className={style.captchaImg}>
+                            <img height="38px" src="https://www.cwagi.com/common/sec/captcha" alt="" />
+                        </div>
+                    </div>
+                    <Button type="primary" className="loginBtn" block onClick={goLogin}>登录</Button>
                     </Space>
                 </div>
             </div>
